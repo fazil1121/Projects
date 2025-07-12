ebcf0674f50662d596ee8767d26ce15ebece508e
fazil1121 <faziil.career@gmail.com>
2025-07-12 18:15:30 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+emotion_to_genre = {
+    "happy": "pop",
+    "sad": "Indian-classical",
+    "angry": "Soul",
+    "neutral": "Hip-hop",
+    "surprise": "edm",
+    "fear": "Folk",
+    "disgust": "metal"
+}
