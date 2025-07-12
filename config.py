6096ca24669116ea15cb5465573d0f1f8cc30bf5
fazil1121 <faziil.career@gmail.com>
2025-07-12 18:17:35 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-emotion_to_genre = {
-    "happy": "pop",
-    "sad": "Indian-classical",
-    "angry": "Soul",
-    "neutral": "Hip-hop",
-    "surprise": "edm",
-    "fear": "Folk",
-    "disgust": "metal"
-}
